617a180273ba4f994af0affacefc1144e51c4db9
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-18 21:35:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -36,14 +36,54 @@ def cantidadDe1EnCromosoma(poblacion):
         fitnessDeCadaCromosoma.append(cantidad1)
     return fitnessDeCadaCromosoma
 
+def funcionDeAdaptacion(CantidadDe1EnCromosoma, CantidadDe1EnCromosomaOrdenado):
+    fitnessDeCadaCromosoma=[]
+    for i in range(0,len(CantidadDe1EnCromosoma)):
+        fitness=CantidadDe1EnCromosomaOrdenado[-1]-CantidadDe1EnCromosoma[i]
+        fitnessDeCadaCromosoma.append(fitness)
+    return fitnessDeCadaCromosoma
+
+def cantidadDe1XCromosomaOrdenado():
+    CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
+    CantidadDe1XCromosomaOrdenado=CantidadDe1XCromosoma.copy()
+    CantidadDe1XCromosomaOrdenado.sort()
+    return CantidadDe1XCromosomaOrdenado
+
+def Seleccionar(Poblacion):
+    CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
+    CantidadDe1XCromosomaOrdenado=cantidadDe1XCromosomaOrdenado()
+    fitness=funcionDeAdaptacion(CantidadDe1XCromosoma,CantidadDe1XCromosomaOrdenado)
+
+    Individuos=[]
+    num=random.randrange(0,len(Poblacion))
+    primerCromosona=Poblacion[num]
+    Individuos.append(primerCromosona)
+
+    num2=random.randrange(0,len(Poblacion))
+    segundoCromosoma=Poblacion[num2]
+    Individuos.append(segundoCromosoma)
+    
+
 ##-----------------------------------------------##
 
-#crear poblacion
+#Crear poblacion
+print("Poblacion de cromosomas: ")
 for i in range(0,TAMPOBLACION):
         insertarCromosomaAPoblacion(TAMGEN)
 print(Poblacion)
 print(len(Poblacion))
+print("\n")
 
-print ("\n")
+#Contar unos de las poblacion de cromosomas
+print("Numero de unos de la problacion de cromosomas: ")
 fitnessXCrosomosa=cantidadDe1EnCromosoma(Poblacion)
 print(fitnessXCrosomosa)
+print("\n")
+
+#
+CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
+CantidadDe1XCromosomaOrdenado=cantidadDe1XCromosomaOrdenado()
+print(CantidadDe1XCromosomaOrdenado)
+print("\n")
+fitness=funcionDeAdaptacion(CantidadDe1XCromosoma,CantidadDe1XCromosomaOrdenado)
+print(fitness)
