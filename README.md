d2b609aceed4976fff09992200c7457cb4d1e842
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-04-20 00:43:03 -0400

    Codigo finalizado - Ejemplo

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # genetic-algo-john
+
+## Algotirmo Genetico
+
+**¿De que trata el proyecto?**
+La premisa de esta practica es hacer correr el algoritmo genetico visto en clases utilizando los metodos: selection, crossover y mutation.  Cada cromosoma x tiene un tamanio de 20 genes y esta formado por 0s y 1s solamente.
+
+Utilizando una probabilidad de crossover pc = 0.7 y una probabilidad de mutation pm = 0.001 para crear nuevas generaciones de cromosomas iniciando con una poblacion inicial de 100 cromosomas generados al azar, hasta que te devuelva la generacion en la cual se encontro el cromosoma mas fuerte.
+
+(Se adjuntaron imagenes para aclarecer mas las dudas)
+
+*Primer Ejemplo*
+
+Ejemplo: NumeroDeGeneraciones.png
+
+
+## Experimentos
+
+**Primer experimento**
+    Ejecutar el algoritmo 20 veces, para cada ejecucion (run) reporta en que generacion se encontro al cromosoma mas fuerte. Reporta la media de este valor.
+
+    Ejemplo: PrimerExperimento.png
+
+**Sefundo experimento**
+    Ejecuta el mismo experimento, pero esta vez sin crossover (pc = 0, pm = 0.001).
+
+    Advertencia: Cabe recalcar que la funcion toma mucho tiempo para hacer correr incluso una iteracion, asi que se dejo corriendolo pero sin muestra alguna, ya que la probabilidad de cossover es 0.
+
+    Ejemplo: SegundoExperimento.png
+
+**Tercer experimento**
+    Ejecuta el mismo experimento, pero esta vez sin mutation (pc = 0.7, pm = 0).
+
+    Ejemplo: TercerExperimento.png
+
+**Cuarto experimento**
+    Ejecuta el mismo experimento, pero esta vez con estos valores:
+
+    * pc = 0.9, pm = 0.001
+        Ejemplo: CuartoExperimento-1.png
+    * pc = 0.3, pm = 0.001
+        Ejemplo: CuartoExperimento-2.png
+    * pc = 0.2, pm = 0.001
+        Ejemplo: CuartoExperimento-3.png
+    * pc = 0.05, pm = 0.001
+        Ejemplo: CuartoExperimento-4.png
+
+
+**Quinto experimento**
+    ¿Cual es la mejor opcion de parametros segun los resultados obtenidos anteriormente? ¿Por que?
+
+        Respuesta: La mejor opcion es la primera, que tiene una pc=0.9, porque con una probabilidad de crossove mas cercana a 1 hay mas probabilidades de que podamos encontrar el cromosoma mas fuerte.
+    
+**Sexto experimento**
+    Ejecuta el algoritmo 20 veces, para cada ejecucion (run) reporta en que generacion se encontro al cromosoma mas fuerte. Reporta la media de este valor. Varıa el tamano de la poblacion. Prueba con 50, 100, 500, 1000 cromosomas. ¿Cual es la mejor opcion? ¿Por que?
+
+    pc = 0.7, pm = 0.001
+
+    * Poblacion:50
+        Ejemplo: SextoExperimento-1.png
+    * Poblacion:100
+        Ejemplo: SextoExperimento-2.png
+    * Poblacion:500
+        Ejemplo: SextoExperimento-3.png
+    * Poblacion:1000
+        Ejemplo: SextoExperimento-4.png
+
+        Respuesta: La mejor opcion es la cuarta, con una poblacion de 1000; porque con una poblacion mas grande tenemos mas probabilidades de encontrar el cromosoma mas fuerte en menos generaciones, ya que tomamos en cuenta mas gente para el experimento.
+    
+**Septimo experimento**
+    Ejecuta el algoritmo por 100 generaciones y grafica el mejor, el peor y el promedio del fitness function encontrado en cada generacion vs el numero de generacion.
+
+
+    Ejemplo: SeptimoExperimento.png
+
+
+
+## Guia
+
+**Instalaciones**
+
+- [Python 3.8.3](https://www.python.org/downloads/release/python-383/)
+- [Visual code](https://code.visualstudio.com/download)
+
+Nota: Visual Code no es completamente necesario, si se quiere se puede utilizar otro editor de codigo que usted elija.
+
+**Pasos para hacer correr el codigo**
+
+Una vez conseguido el codigo hacer lo siguiente:
+
+    *Abrir una terminal con la siguiente ruta como ejemplo: "C:\Users\PC\Desktop\Sistemas Inteligentes\algoritmo_genetico\" (Sin las comillas)
+    *En la terminal escribir la siguiente linea: "py main.py" (Sin las comillas)
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from random import random, choice
-Poblacion=[]
+from random import choice
+from random import choices
+import random as rand
 TAMGEN=20
 TAMPOBLACION=100
-
-pc=0.7 #probabilidad de cruce
-pm=0.001 #probabilidad de mutación
 
 ##-------------------FUNCIONES-------------------##
 
@@ -14,12 +12,12 @@ def insertarGen(tamgen):
         cromosoma.append(choice([0,1]))
     return cromosoma
 
-def insertarPoblacion(cromosoma):
+def insertarPoblacion(cromosoma, Poblacion):
     Poblacion.append(cromosoma)
 
-def insertarCromosomaAPoblacion(TAMGEN):
+def insertarCromosomaAPoblacion(TAMGEN, Poblacion):
     cromosoma=insertarGen(TAMGEN)
-    insertarPoblacion(cromosoma)
+    insertarPoblacion(cromosoma, Poblacion)
 
 def contarCant1EnGen(gen):
     cont=0
@@ -36,54 +34,137 @@ def cantidadDe1EnCromosoma(poblacion):
         fitnessDeCadaCromosoma.append(cantidad1)
     return fitnessDeCadaCromosoma
 
-def funcionDeAdaptacion(CantidadDe1EnCromosoma, CantidadDe1EnCromosomaOrdenado):
-    fitnessDeCadaCromosoma=[]
-    for i in range(0,len(CantidadDe1EnCromosoma)):
-        fitness=CantidadDe1EnCromosomaOrdenado[-1]-CantidadDe1EnCromosoma[i]
-        fitnessDeCadaCromosoma.append(fitness)
-    return fitnessDeCadaCromosoma
-
-def cantidadDe1XCromosomaOrdenado():
-    CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
-    CantidadDe1XCromosomaOrdenado=CantidadDe1XCromosoma.copy()
-    CantidadDe1XCromosomaOrdenado.sort()
-    return CantidadDe1XCromosomaOrdenado
+def Seleccionar2(chromosomes , chromosomes_fitness):
+    
+    return choices(population=chromosomes, weights=chromosomes_fitness, k=2)
 
 def Seleccionar(Poblacion):
-    CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
-    CantidadDe1XCromosomaOrdenado=cantidadDe1XCromosomaOrdenado()
-    fitness=funcionDeAdaptacion(CantidadDe1XCromosoma,CantidadDe1XCromosomaOrdenado)
-
     Individuos=[]
-    num=random.randrange(0,len(Poblacion))
+    num=rand.randrange(0,len(Poblacion))
     primerCromosona=Poblacion[num]
     Individuos.append(primerCromosona)
 
-    num2=random.randrange(0,len(Poblacion))
+    num2=rand.randrange(0,len(Poblacion))
     segundoCromosoma=Poblacion[num2]
     Individuos.append(segundoCromosoma)
+    return Individuos
+
+def Cruzar(ParDeIndividuos):
+    probDeCruzarrActual=rand.random()
+
+    if(probDeCruzarrActual<=pc):
+
+        #num=rand.randrange(1, TAMGEN/2) #Tamaño de cromosomas a intercambiar
+        #numSegundoFor=num
+        #InicioPrimerCorte=rand.randrange(0, TAMGEN/2)
+        #InicioSegundoCorte=rand.randrange(TAMGEN/2, TAMGEN-num)
+        #listaCromosomasAIntercamibiar=[]
+        #listaCromosomasAIntercamibiar2=[]
+        #listaAux=[]
+        
+        #primerCorte=InicioPrimerCorte
+        #segundoCorte=InicioSegundoCorte
+        #for i in range (0,num):
+        #    num1=ParDeIndividuos[0][primerCorte]
+        #    
+        #    listaCromosomasAIntercamibiar.append(num1)
+        #    primerCorte=primerCorte+1
+            
+        #    num2=ParDeIndividuos[1][segundoCorte]
+            
+        #    listaCromosomasAIntercamibiar2.append(num2)
+        #    segundoCorte=segundoCorte+1
+
+        #listaAux=listaCromosomasAIntercamibiar.copy()
+        #listaCromosomasAIntercamibiar=listaCromosomasAIntercamibiar2
+        #listaCromosomasAIntercamibiar2=listaAux
+        
+        #primerCorte2=InicioPrimerCorte
+        #segundoCorte2=InicioSegundoCorte
+
+        #for i in range(0,numSegundoFor):
+        #   ParDeIndividuos[0][primerCorte2] = listaCromosomasAIntercamibiar[i]
+        #  primerCorte2=primerCorte2+1
+        #for i in range(0,numSegundoFor):
+        #    ParDeIndividuos[1][segundoCorte2]= listaCromosomasAIntercamibiar2[i]
+        #    segundoCorte2=segundoCorte2+1
+
+        num=rand.randrange(1, TAMGEN-1)
+        cromIntercambiar1=[]
+        cromIntercambiar2=[]
+        cromIntercambiar1=ParDeIndividuos[0][:num] + ParDeIndividuos[1][num:]
+        cromIntercambiar2=ParDeIndividuos[1][:num] + ParDeIndividuos[0][num:]
+        ParDeIndividuos[0]=cromIntercambiar1
+        ParDeIndividuos[1]=cromIntercambiar2
+    return ParDeIndividuos
+
+def Mutar(DosHijos):
+    probDeMutarActual=rand.random()
+    cromosomaAMutar=rand.randrange(0, TAMGEN)
+
+    if(probDeMutarActual<=pm):
+        if(DosHijos[0][cromosomaAMutar]==0) and (DosHijos[1][cromosomaAMutar]==0):
+            DosHijos[0][cromosomaAMutar]=1
+            DosHijos[1][cromosomaAMutar]=1
+        elif(DosHijos[0][cromosomaAMutar]==1) and (DosHijos[1][cromosomaAMutar]==1):
+            DosHijos[0][cromosomaAMutar]=0
+            DosHijos[1][cromosomaAMutar]=0
+        elif(DosHijos[0][cromosomaAMutar]==1) and (DosHijos[1][cromosomaAMutar]==0):
+            DosHijos[0][cromosomaAMutar]=0
+            DosHijos[1][cromosomaAMutar]=1
+        elif(DosHijos[0][cromosomaAMutar]==0) and (DosHijos[1][cromosomaAMutar]==1):
+            DosHijos[0][cromosomaAMutar]=1
+            DosHijos[1][cromosomaAMutar]=0
+    return DosHijos
+
+def Converge(Poblacion):
+    fitness = cantidadDe1EnCromosoma(Poblacion)
+    resp=False
+    limite=len(Poblacion)
+    for i in range (0,limite):
+        if(fitness[i]==20):
+            resp=True
+    return resp
+
+def Reducir(Poblacion):        
+    fitness = cantidadDe1EnCromosoma(Poblacion)
+    posicion=0
+    tamlimite=len(Poblacion)
     
+    while tamlimite>=100:
+        num=rand.randrange(0, tamlimite)
+        fitness.pop(num)
+        Poblacion.pop(num)    
+        tamlimite=tamlimite-1
+
+def AlgoritmoGeneticoSimple(Poblacion):
+    cont=0
+    for i in range(0,TAMPOBLACION):
+        insertarCromosomaAPoblacion(TAMGEN,Poblacion)
+    
+    while (Converge(Poblacion)==False):
+        nueva_Poblacion=[]
+        fitness=cantidadDe1EnCromosoma(Poblacion)
+        
+        for j in range(0,int(TAMPOBLACION/2)):
+            DosIndividuos=Seleccionar2(Poblacion,fitness)
+            DosHijos=Cruzar(DosIndividuos)
+            DosHijos=Mutar(DosHijos)
+            nueva_Poblacion.append(DosHijos[0])
+            nueva_Poblacion.append(DosHijos[1])
+        Poblacion=nueva_Poblacion
+        cont=cont+1
+    return cont,Poblacion
+
 
 ##-----------------------------------------------##
 
-#Crear poblacion
-print("Poblacion de cromosomas: ")
-for i in range(0,TAMPOBLACION):
-        insertarCromosomaAPoblacion(TAMGEN)
-print(Poblacion)
-print(len(Poblacion))
-print("\n")
+if __name__ == "__main__" :
+    Poblacion=[]
+    pc=0.7 #probabilidad de cruce
+    pm=0.001 #probabilidad de mutación
+    resp,resp2=AlgoritmoGeneticoSimple(Poblacion)
+    print("Numero de eneraciones para encontrar el cromosoma mas fuerte: ")
+    print(resp)
+  
 
-#Contar unos de las poblacion de cromosomas
-print("Numero de unos de la problacion de cromosomas: ")
-fitnessXCrosomosa=cantidadDe1EnCromosoma(Poblacion)
-print(fitnessXCrosomosa)
-print("\n")
-
-#
-CantidadDe1XCromosoma=cantidadDe1EnCromosoma(Poblacion)
-CantidadDe1XCromosomaOrdenado=cantidadDe1XCromosomaOrdenado()
-print(CantidadDe1XCromosomaOrdenado)
-print("\n")
-fitness=funcionDeAdaptacion(CantidadDe1XCromosoma,CantidadDe1XCromosomaOrdenado)
-print(fitness)
